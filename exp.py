e7d2e34d16977eee73a6bb03b79c02fd796956f7
mkearne <mkearne@hotmail.com>
2016-03-07 20:25:39 -0600

    Added files via upload

---
+++ exp.py
@@ -0,0 +1,38 @@
+print "Hello World"
+print "yeah this is super cool"
+print "I can do python"
+print #
+print 'I "said" dont fuck with me'
+
+five = 10 + 5 + 5 - 10 - 5
+print " here is the number five %s" % five
+
+def secret_formula(test):
+	beer = test/10
+	gin = "hello"
+	return beer, gin
+
+	
+def another_great_job(var1):
+	big_number = 100000/10 * 2
+	little_number  = (var1*5)
+	littler_number = (var2 * 3.14)
+	return big_number, little_number, littler_number
+	
+test = 100
+beer, gin = secret_formula(test)
+print "check this out %d beer, %s gin" % (beer, gin)
+
+a,b = (0,1)
+while b < 1000: 
+	print b,
+	a,b = b, a+b 
+	
+range (1,10)
+	
+	
+
+
+
+
+
